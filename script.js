d34e2d997d59bd76d11a006b6e1beb7ba5ee3280
drewatienza <atienza.drew@gmail.com>
2020-12-14 09:12:31 -0800

    implemented infinite scroll functionality

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
 // Unsplash API
-const count = 10;
+const count = 30;
 const apiKey = 'uBqpE-QzSCmyS6f5W_rGlO54z_2yt8A5DUIoK1blzjA';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Check if all images were loaded
+const imageLoaded = () => {
+  imagesLoaded++;
+  if(imagesLoaded === totalImages) {
+    ready = true;
+    loader.hidden = true;
+  }
+}
 
 // Helper Function to Set Attributes on DOM Elements
 const setAttributes = (element, attributes) => {
@@ -17,11 +29,12 @@ const setAttributes = (element, attributes) => {
 
 // Create Elements For Links & Photos, Add to DOM
 const displayPhotos = () => {
+  imagesLoaded = 0;
+  totalImages = photosArray.length;
   // Run function for each object in photosArray
   photosArray.forEach((photo) => {
     // Create <a> to link to Unsplash
     const item = document.createElement('a');
-    // item.setAttribute('href', photo.links.ht
     setAttributes(item, {
       href: photo.links.html,
       target: '_blank',
@@ -33,6 +46,8 @@ const displayPhotos = () => {
       alt: photo.alt_description,
       title: photo.alt_description,
     });
+    // Event Listener, check when each is finished loading
+    img.addEventListener('load', imageLoaded);
     // Put <img> inside <a>, then put both inside image container element
     item.appendChild(img);
     imageContainer.appendChild(item);
@@ -49,6 +64,14 @@ const getPhotos = async () => {
     // Catch Error Here
   }
 }
+
+// Check to see if scrolling near bottom of page, Load More Photos
+window.addEventListener('scroll', () => {
+  if(window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+    ready = false;
+    getPhotos();
+  }
+});
 
 // On Load
 getPhotos();
