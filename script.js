28ffa0a4788620cfcb944cf35ea2c02e66af8377
drewatienza <atienza.drew@gmail.com>
2020-12-14 09:18:33 -0800

    made load performance tweak and finished application

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ let totalImages = 0;
 let photosArray = [];
 
 // Unsplash API
-const count = 30;
+let count = 5;
 const apiKey = 'uBqpE-QzSCmyS6f5W_rGlO54z_2yt8A5DUIoK1blzjA';
-const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+let apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
 // Check if all images were loaded
 const imageLoaded = () => {
@@ -17,6 +17,7 @@ const imageLoaded = () => {
   if(imagesLoaded === totalImages) {
     ready = true;
     loader.hidden = true;
+    count = 30;
   }
 }
 
